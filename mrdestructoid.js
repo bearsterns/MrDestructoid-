4e3e82d697e238ea0fc51d28b5dd0cc2497256c6
Siddarth Patel <siddarthpatel22@gmail.com>
2021-01-16 00:39:07 +0530

    Initial template

---
+++ mrdestructoid.js
@@ -0,0 +1,46 @@
+const tmi = require('tmi.js');
+
+// Define configuration options
+const opts = {
+  identity: {
+    username: {/* <BOT_USERNAME> */},
+    password: {/* <OAUTH_TOKEN> */}
+  },
+  channels: [
+    {/* <CHANNEL_NAME> */}
+  ]
+};
+// Create a client with our options
+const client = new tmi.client(opts);
+
+// Register our event handlers (defined below)
+client.on('message', onMessageHandler);
+client.on('connected', onConnectedHandler);
+
+// Connect to Twitch:
+client.connect();
+
+// Called every time a message comes in
+function onMessageHandler (target, context, msg, self) {
+  if (self) { return; } // Ignore messages from the bot
+
+  // Remove whitespace from chat message
+  const commandName = msg.trim();
+
+  // If the command is known, let's execute it
+  if (commandName === '!ping') {
+    const res = ping();
+    client.say(target, res);
+    console.log(`* Executed ${commandName} command`);
+  } else {
+    console.log(`* Unknown command ${commandName}`);
+  }
+}
+// Function called when the "ping" command is issued
+function ping () {
+  return "KKona 7";
+}
+// Called every time the bot connects to Twitch chat
+function onConnectedHandler (addr, port) {
+  console.log(`* Connected to ${addr}:${port}`);
+}
